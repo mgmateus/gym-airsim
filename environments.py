0c9ffceeeb17af6d6b577fd2ac6060870324c38c
mgmateus <eng.mgmateus@gmail.com>
2024-02-15 12:14:26 -0300

    6-DoF working

--- environments.py
+++ environments.py
@@ -1,10 +1,30 @@
 from gym import Env, spaces
 
-from airsim_resources.simulation import QuarotorROS
+from airsim_resources.simulation import ComputerVision, QuadrotorClient
+# from airsim_resources.noetic import QuarotorROS
 
-class Environment(Env):
-    def __init__(self, vehicle_name):
-        self.vehicle = QuarotorROS(vehicle_name)
+
+class EnvironmentCV(Env):
+    def __init__(self, ip : str):
+        self.vehicle = ComputerVision(ip)
         
     def step(self, action):
         self.vehicle.get_state(action)
+
+class EnvironmentQuadrotor(Env):
+    def __init__(self, ip : str):
+        self.vehicle = QuadrotorClient(ip)
+        
+    def step(self, action):
+        self.vehicle.get_state(action)
+
+
+
+
+# class EnvironmentROS(Env):
+#     def __init__(self, vehicle_name):
+#         self.vehicle = QuarotorROS(vehicle_name)
+        
+#     def step(self, action):
+#         self.vehicle.get_state(action)
+
---
+++ mxscurl.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import time
+import os
+
+import numpy as np
+
+from environments import EnvironmentCV, EnvironmentQuadrotor
+
+if __name__ == "__main__":
+    ip = os.environ['UE4_IP']
+    env = EnvironmentQuadrotor(ip)
+    
+    env.vehicle.take_off()
+    action = np.array([-.5, .9, -1, 0.01, 0.001, -0.001])
+    for i in range(30000):
+        env.step(action)
+
+    
+        
