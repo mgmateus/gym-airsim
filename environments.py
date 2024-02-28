0669261d22488dc6ab0cc97f8cf49b9289a4d05f
mgmateus <eng.mgmateus@gmail.com>
2024-02-28 09:23:26 -0300

    line of sigh estimation

--- environments.py
+++ environments.py
@@ -1,7 +1,7 @@
 from gym import Env, spaces
 
 # from airsim_resources.simulation import ComputerVision, QuadrotorClient
-from .airsim_resources.noetic import QuarotorStereoROS
+from .airsim_resources.noetic import Position, Trajectory
 
 
 # class EnvironmentCV(Env):
@@ -19,20 +19,13 @@ from .airsim_resources.noetic import QuarotorStereoROS
 #         self.vehicle.get_state(action)
 
 
-class EnvironmentROS(Env):
+class EnvironmentPositionNBV(Env):
     def __init__(self, ip : str, 
                  vehicle_name : str, 
                  camera_name : str, 
-                 observation : str,
-                 vertices_path : str, 
-                 vertices_name : str):
+                 observation_type : str):
         
-        self.vehicle = QuarotorStereoROS(ip, 
-                                         vehicle_name, 
-                                         camera_name, 
-                                         observation, 
-                                         vertices_path, 
-                                         vertices_name)
+        self.vehicle = Position(ip, vehicle_name, camera_name, observation_type)   
         
     def step(self, action):
         state = self.vehicle.get_state(action)
