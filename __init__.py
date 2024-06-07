e3779c4df12c53e6a978945cc23889d221b2e639
mgmateus <eng.mgmateus@gmail.com>
2024-06-06 21:48:01 -0300

    Env register on __init__

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+from gymnasium.envs.registration import register
 
+register(
+    id='gym-airsim/eolic-aereo-point-of-view',
+    entry_point='environments:AereoPointOfView'
+)
+
+register(
+    id='gym-airsim/eolic-underwater-point-of-view',
+    entry_point='environments:UnderwaterPointOfView'
+)
