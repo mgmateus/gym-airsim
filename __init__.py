90efd8a09eff2b1385c2c2e6c04b4f1899ee0397
mgmateus <eng.mgmateus@gmail.com>
2024-07-08 16:05:41 -0300

    Addin basic environment.

--- __init__.py
+++ __init__.py
@@ -12,3 +12,13 @@ register(
     id='gym-airsim/eolic-underwater-point-of-view',
     entry_point='gym-airsim.environments:UnderwaterPointOfView'
 )
+
+register(
+    id='gym-airsim/basic-aereo-point-of-view',
+    entry_point='gym-airsim.environments:AereoPointOfView'
+)
+
+register(
+    id='gym-airsim/basic-underwater-point-of-view',
+    entry_point='gym-airsim.environments:UnderwaterPointOfView'
+)
